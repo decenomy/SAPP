44bc8ded0c3f161e81afd7294f5195ce7abc1523
furszy <matiasfurszyfer@protonmail.com>
2020-05-11 17:52:14 -0300

    Backport utiltest files

---
+++ src/test/librust/utiltest.h
@@ -0,0 +1,38 @@
+// Copyright (c) 2016-2020 The Zcash developers
+// Copyright (c) 2020 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://www.opensource.org/licenses/mit-license.php .
+
+#ifndef PIVX_UTIL_TEST_H
+#define PIVX_UTIL_TEST_H
+
+#include "sapling/address.hpp"
+#include "sapling/incrementalmerkletree.hpp"
+#include "sapling/note.hpp"
+#include "sapling/noteencryption.hpp"
+#include "wallet/wallet.h"
+
+struct TestSaplingNote {
+    libzcash::SaplingNote note;
+    SaplingMerkleTree tree;
+};
+
+const Consensus::Params& RegtestActivateSapling();
+
+void RegtestDeactivateSapling();
+
+libzcash::SaplingExtendedSpendingKey GetTestMasterSaplingSpendingKey();
+
+CKey AddTestCKeyToKeyStore(CBasicKeyStore& keyStore);
+
+/**
+ * Generate a dummy SaplingNote and a SaplingMerkleTree with that note's commitment.
+ */
+TestSaplingNote GetTestSaplingNote(const libzcash::SaplingPaymentAddress& pa, CAmount value);
+
+CWalletTx GetValidSaplingReceive(const Consensus::Params& consensusParams,
+                                 CBasicKeyStore& keyStore,
+                                 const libzcash::SaplingExtendedSpendingKey &sk,
+                                 CAmount value);
+
+#endif // PIVX_UTIL_TEST_H
