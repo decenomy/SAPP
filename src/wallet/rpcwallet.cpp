048f5e7c6bb0bdb935fb7320617c90cc23ba769e
furszy <matiasfurszyfer@protonmail.com>
2020-05-27 11:18:38 -0300

    rpc: getshieldedbalance command implemented.

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -1119,6 +1119,56 @@ UniValue sendtoaddressix(const JSONRPCRequest& request)
     return wtx.GetHash().GetHex();
 }
 
+CAmount getBalanceShieldedAddr(std::string address, int minDepth = 1, bool ignoreUnspendable=true) {
+    CAmount balance = 0;
+    std::vector<SaplingNoteEntry> saplingEntries;
+    LOCK2(cs_main, pwalletMain->cs_wallet);
+    libzcash::PaymentAddress filterAddress = KeyIO::DecodePaymentAddress(address);
+    pwalletMain->GetSaplingScriptPubKeyMan()->GetFilteredNotes(saplingEntries, filterAddress, minDepth, true, ignoreUnspendable);
+    for (auto & entry : saplingEntries) {
+        balance += CAmount(entry.note.value());
+    }
+    return balance;
+}
+
+UniValue getshieldedbalance(const JSONRPCRequest& request)
+{
+    if (!pwalletMain)
+        return NullUniValue;
+
+    if (request.fHelp || request.params.size() > 2)
+        throw std::runtime_error(
+                "getshieldedbalance ( minconf includeWatchonly )\n"
+                "\nReturn the total shielded value of funds stored in the node's wallet.\n"
+                "\nCAUTION: If the wallet contains any addresses for which it only has incoming viewing keys,"
+                "\nthe returned private balance may be larger than the actual balance, because spends cannot"
+                "\nbe detected with incoming viewing keys.\n"
+                "\nArguments:\n"
+                "1. minconf          (numeric, optional, default=1) Only include private and transparent transactions confirmed at least this many times.\n"
+                "2. includeWatchonly (bool, optional, default=false) Also include balance in watchonly addresses (see 'importaddress' and 'z_importviewingkey')\n"
+                "\nResult:\n"
+                "amount              (numeric) the total balance of shielded funds (in Sapling addresses)\n"
+                "\nExamples:\n"
+                "\nThe total amount in the wallet\n"
+                + HelpExampleCli("getshieldedbalance", "") +
+                "\nThe total amount in the wallet at least 5 blocks confirmed\n"
+                + HelpExampleCli("getshieldedbalance", "5") +
+                "\nAs a json rpc call\n"
+                + HelpExampleRpc("getshieldedbalance", "5")
+        );
+
+    LOCK2(cs_main, pwalletMain->cs_wallet);
+
+    const int nMinDepth = request.params.size() > 0 ? request.params[0].get_int() : 1;
+    if (nMinDepth < 0) {
+        throw JSONRPCError(RPC_INVALID_PARAMETER, "Minimum number of confirmations cannot be less than 0");
+    }
+
+    const bool fIncludeWatchonly = (request.params.size() > 1) && request.params[1].get_bool();
+    CAmount nBalance = getBalanceShieldedAddr("", nMinDepth, !fIncludeWatchonly);
+    return ValueFromAmount(nBalance);
+}
+
 UniValue listaddressgroupings(const JSONRPCRequest& request)
 {
     if (request.fHelp)
@@ -3358,6 +3408,7 @@ static const CRPCCommand commands[] =
     { "wallet",             "importsaplingkey",         &importsaplingkey,         true  },
     { "wallet",             "importsaplingviewingkey",  &importsaplingviewingkey,  true  },
     { "wallet",             "exportsaplingviewingkey",  &exportsaplingviewingkey,  true  },
+    { "wallet",             "getshieldedbalance",       &getshieldedbalance,       false },
 
     /** Label functions (to replace non-balance account functions) */
     { "wallet",             "getaddressesbylabel",      &getaddressesbylabel,      true  },
