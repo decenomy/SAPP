bfa4db41ba53c11d5e8b3019a8c5112f8fbb5029
furszy <matiasfurszyfer@protonmail.com>
2020-07-07 13:34:24 -0300

    Sapling transaction fixed fee check.

--- src/validation.cpp
+++ src/validation.cpp
@@ -515,10 +515,21 @@ bool AcceptToMemoryPoolWorker(CTxMemPool& pool, CValidationState &state, const C
             }
         }
 
-        if (fRejectAbsurdFee && nFees > ::minRelayTxFee.GetFee(nSize) * 10000)
-            return state.Invalid(false,
-                REJECT_HIGHFEE, "absurdly-high-fee",
-                strprintf("%d > %d", nFees, ::minRelayTxFee.GetFee(nSize) * 10000));
+        if (!tx.hasSaplingData()) { // Sapling fee could be higher. Review it properly
+            if (fRejectAbsurdFee && nFees > ::minRelayTxFee.GetFee(nSize) * 10000) {
+                return state.Invalid(false,
+                                     REJECT_HIGHFEE, "absurdly-high-fee",
+                                     strprintf("%d > %d", nFees, ::minRelayTxFee.GetFee(nSize) * 10000));
+            }
+        } else {
+            // Accept a tx if it contains sapling and has at least the default fee specified (0.0001 PIV).
+            // todo: this is a initial step, we can definitely be more accurate with the fee calculation and use the regular fee flow.
+            if (nFees < 10000) {
+                return state.Invalid(false,
+                                     REJECT_HIGHFEE, "sapling-invalid-fee",
+                                     strprintf("sapling fee: %d < %d", nFees, 10000));
+            }
+        }
 
         // As zero fee transactions are not going to be accepted in the near future (4.0) and the code will be fully refactored soon.
         // This is just a quick inline towards that goal, the mempool by default will not accept them. Blocking
