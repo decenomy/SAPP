520d889d7f8c7a184c66fbe9285ae84bbc4aac70
furszy <matiasfurszyfer@protonmail.com>
2020-09-23 23:42:08 -0300

    BugFix: g_IsSaplingActive flag wasn't being updated at startup.

--- src/validation.cpp
+++ src/validation.cpp
@@ -3600,6 +3600,10 @@ bool static LoadBlockIndexDB(std::string& strError)
     PruneBlockIndexCandidates();
 
     const CBlockIndex* pChainTip = chainActive.Tip();
+
+    // initial global flag update
+    g_IsSaplingActive = Params().GetConsensus().NetworkUpgradeActive(pChainTip->nHeight, Consensus::UPGRADE_V5_DUMMY);
+
     LogPrintf("LoadBlockIndexDB(): hashBestChain=%s height=%d date=%s progress=%f\n",
             pChainTip->GetBlockHash().GetHex(), pChainTip->nHeight,
             DateTimeStrFormat("%Y-%m-%d %H:%M:%S", pChainTip->GetBlockTime()),
