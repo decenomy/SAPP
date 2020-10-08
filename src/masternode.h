f5349b975f67097fca7897bbe513db6f272224b2
random-zebra <random.zebra@protonmail.com>
2020-10-08 16:24:12 +0200

    [Cleanup] Remove global map mapCacheBlockHashes

--- src/masternode.h
+++ src/masternode.h
@@ -17,7 +17,6 @@
 class CMasternode;
 class CMasternodeBroadcast;
 class CMasternodePing;
-extern std::map<int64_t, uint256> mapCacheBlockHashes;
 
 int MasternodeMinPingSeconds();
 int MasternodeBroadcastSeconds();
