4aef2f30bb522dfd57c1a44fa7643f8afc56ee63
Fuzzbawls <fuzzbawls@gmail.com>
2020-10-22 02:51:43 -0700

    Fix constness of ArgsManager methods

--- src/util.h
+++ src/util.h
@@ -105,14 +105,21 @@ inline bool IsSwitchChar(char c)
 class ArgsManager
 {
 protected:
-    RecursiveMutex cs_args;
+    mutable RecursiveMutex cs_args;
     std::map<std::string, std::string> mapArgs;
-    std::map<std::string, std::vector<std::string> > mapMultiArgs;
+    std::map<std::string, std::vector<std::string>> mapMultiArgs;
 
 public:
     void ParseParameters(int argc, const char* const argv[]);
     void ReadConfigFile();
-    std::vector<std::string> GetArgs(const std::string& strArg);
+
+    /**
+     * Return a vector of strings of the given argument
+     *
+     * @param strArg Argument to get (e.g. "-foo")
+     * @return command-line arguments
+     */
+    std::vector<std::string> GetArgs(const std::string& strArg) const;
 
     /**
     * Return true if the given argument has been manually set
@@ -120,7 +127,7 @@ public:
     * @param strArg Argument to get (e.g. "-foo")
     * @return true if the argument has been set
     */
-    bool IsArgSet(const std::string& strArg);
+    bool IsArgSet(const std::string& strArg) const;
 
     /**
     * Return string argument or default value
@@ -129,7 +136,7 @@ public:
     * @param default (e.g. "1")
     * @return command-line argument or default value
     */
-    std::string GetArg(const std::string& strArg, const std::string& strDefault);
+    std::string GetArg(const std::string& strArg, const std::string& strDefault) const;
 
     /**
     * Return integer argument or default value
@@ -138,7 +145,7 @@ public:
     * @param default (e.g. 1)
     * @return command-line argument (0 if invalid number) or default value
     */
-    int64_t GetArg(const std::string& strArg, int64_t nDefault);
+    int64_t GetArg(const std::string& strArg, int64_t nDefault) const;
 
     /**
     * Return boolean argument or default value
@@ -147,7 +154,7 @@ public:
     * @param default (true or false)
     * @return command-line argument or default value
     */
-    bool GetBoolArg(const std::string& strArg, bool fDefault);
+    bool GetBoolArg(const std::string& strArg, bool fDefault) const;
 
     /**
     * Set an argument if it doesn't already have a value
