4e99c268061473584bf96540eea433374e6439df
Bedri Özgür Güler <bedriguler@gmail.com>
2020-11-13 15:21:35 +0300

    First changes for Sapphire parameters

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -22,7 +22,7 @@ public:
     CBaseMainParams()
     {
         networkID = CBaseChainParams::MAIN;
-        nRPCPort = 51473;
+        nRPCPort = 45329;
     }
 };
 static CBaseMainParams mainParams;
@@ -36,7 +36,7 @@ public:
     CBaseTestNetParams()
     {
         networkID = CBaseChainParams::TESTNET;
-        nRPCPort = 51475;
+        nRPCPort = 46329;
         strDataDir = "testnet4";
     }
 };
