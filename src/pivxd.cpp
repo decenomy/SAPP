2074fa330979444747d77d1dcd0cd6a992c953ed
Bedri Özgür Güler <bedriguler@gmail.com>
2020-12-29 18:22:58 +0100

    Replace 'PIVX server starting' message with 'Sapphire server starting' message

--- src/pivxd.cpp
+++ src/pivxd.cpp
@@ -120,7 +120,7 @@ bool AppInit(int argc, char* argv[])
 #ifndef WIN32
         fDaemon = GetBoolArg("-daemon", false);
         if (fDaemon) {
-            fprintf(stdout, "PIVX server starting\n");
+            fprintf(stdout, "Sapphire server starting\n");
 
             // Daemonize
             pid_t pid = fork();
