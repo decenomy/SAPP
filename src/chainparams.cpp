eb54510d7742c0fd7d1fb06c19b8707ada8ceb5a
Bedri Özgür Güler <bedriguler@gmail.com>
2021-01-02 19:28:11 +0100

    Sets consensus.nStakeMinDepth to 100 for testnet

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -341,7 +341,7 @@ public:
         consensus.nPoolMaxTransactions = 3;
         consensus.nProposalEstablishmentTime = 60 * 60 * 24;    // must be at least a day old to make it into a budget
         consensus.nStakeMinAge = 60 * 60;
-        consensus.nStakeMinDepth = 600;
+        consensus.nStakeMinDepth = 100;
         consensus.nTargetTimespan = 1 * 60;
         consensus.nTargetTimespanV2 = consensus.nTargetTimespan;
         consensus.nTargetSpacing = 1 * 60;
