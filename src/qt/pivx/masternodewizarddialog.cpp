3ba895697589eb908635f6f74b192078c5df72d9
sapphire-pt <sapphire_pt@sappcoin.com>
2021-01-09 11:09:52 +0000

    fixes the amount used on the masternode creation wizard

--- src/qt/pivx/masternodewizarddialog.cpp
+++ src/qt/pivx/masternodewizarddialog.cpp
@@ -213,7 +213,7 @@ bool MasterNodeWizardDialog::createMN()
         SendCoinsRecipient sendCoinsRecipient(
                 QString::fromStdString(dest.ToString()),
                 QString::fromStdString(alias),
-                CAmount(10000) * COIN,
+                CAmount(CMasternode::GetMasternodeCollateral(chainActive.Height())),
                 "");
 
         // Send the 10 tx to one of your address
