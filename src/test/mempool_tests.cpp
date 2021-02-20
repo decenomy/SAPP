47de27c7820f5fc78e556cfb6d15e313fff71d4f
perseu <jmsaguas@gmail.com>
2021-02-20 15:34:58 +0100

    Added Copyright informations.

--- src/test/mempool_tests.cpp
+++ src/test/mempool_tests.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2011-2014 The Bitcoin Core developers
 // Copyright (c) 2020-2021 The Sapphire Core Developers
+// Copyright (c) 2019 The DECENOMY Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
